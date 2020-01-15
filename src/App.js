0fdbbf6d3119968032c537546fb61284aea10e11
Susan E. Johnson <seagull62@icloud.com>
2020-01-14 22:34:31 -0800

    new function references

--- src/App.js
+++ src/App.js
@@ -1,41 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import data from './data.js';
 
-console.log(data);
-function RenderProducts( ) {
-  return(
-    <div className="Product">
-      <h2>{data.products.length} Products</h2>
-      {data.products.map( (product, idx)=> {
-        return(
-          <li>{product.name}</li>
-        )
-      })}
-    </div>
-  )
-}
+import RenderProducts from './RenderProducts.js';
+import RenderCompanies from './RenderCompanies.js';
+import RenderUsers from './RenderUsers.js';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <RenderProducts />
+      <RenderUsers />
+      <RenderCompanies />
+    </main>
   );
 }
 
-export default RenderProducts;
+export default App;
