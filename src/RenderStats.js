6d841814a94120a5e68811040d9839f4736c585a
Susan E. Johnson <seagull62@icloud.com>
2020-01-16 12:22:09 -0800

    Renders required stats

--- src/RenderStats.js
+++ src/RenderStats.js
@@ -8,7 +8,6 @@ function groupUsersByState(){
   const userObj = {};
   const numUsers = data.users.length;
   let i;
-  console.log("Group these ",data.users.length," users by state.");
   for( i = 0; i < numUsers; i++ ) {
     const state = data.users[i].state;
     if( userObj[state] ) {
@@ -21,50 +20,107 @@ function groupUsersByState(){
     }
   }
   return userObj;
-
 } //end groupUsersByState
 
 function groupCompaniesByState(){
+  //returns object where key is a state
+  //value for each key is the array of companies in that state
   const compObjs = {};
+  const numCompanies = data.companies.length;
+  let i;
+  console.log("Group these ",numCompanies," users by state.");
+  for( i = 0; i < numCompanies; i++ ){
+    const state = data.companies[i].state;
+    if( compObjs[state] ) {
+      //add company to the current group
+      compObjs[state].push(data.companies[i]);
+    } else {
+      //start a new group
+      compObjs[state] = [data.companies[i]];
+    }
+  }
   return compObjs;
-}
+} //end groupCompaniesByState
+
 function groupProductsByMaterial(){
+  //returns object where key is a material
+  //value for each key is the array of products that use that material
   const prodObjs = {};
+  const numProducts = data.products.length;
+  let i;
+  console.log("Group these ",numProducts," products by material.");
+  for( i = 0; i < numProducts; i++ ){
+    const material = data.products[i].material;
+    if( prodObjs[material] ){
+      //add product to the current group
+      prodObjs[material].push(data.products[i]);
+    } else {
+      //start a new group
+      prodObjs[material] = [data.products[i]];
+    }
+  }
   return prodObjs;
-}
+} //end groupProductsByMaterial
 
-function RenderStats() {
-  // Display the state with the most users.
+function RenderStats(){
+  // Displays the following statistics:
+  //    The state with the most users.
+  //    The state with the most companies.
+  //    The most common material for products.
+
   const usersByStateObj = groupUsersByState();
-
-  console.log(usersByStateObj);
     //Find state with the most users
     let mostUsers = 0;
     let savedKeys = [];
     for( const key in usersByStateObj ){
       let count = usersByStateObj[key].length;
-
       if( count > mostUsers ) {
         mostUsers = count;
-        savedKeys = [key]; //re-initialize saved key array;
-      } else if( count === mostUsers) {
+        savedKeys = [key]; //re-initialize saved key array
+      } else if( count === mostUsers ) {
         savedKeys.push(key); //add key to array
       }
     }
 
-
-  // Display the state with the most companies.
   const compByStateObjs = groupCompaniesByState();
-  console.log(compByStateObjs);
+    //Find state with the most companies
+    let mostCompanies = 0;
+    let savedCompanyKeys = [];
+    for( const key in compByStateObjs ){
+      let count = compByStateObjs[key].length;
+      if( count > mostCompanies ) {
+        mostCompanies = count;
+        savedCompanyKeys = [key]; //re-initialize saved key array
+      } else if( count === mostCompanies ){
+        savedCompanyKeys.push(key); //add key to array
+      }
+    }
 
-  // Display the most common material for products.
   const productByMaterialObjs = groupProductsByMaterial();
-  console.log(productByMaterialObjs);
+    //Find the most common material used in the products
+    let mostProducts = 0;
+    let savedProductKeys = [];
+    for( const key in productByMaterialObjs ) {
+      let count = productByMaterialObjs[key].length;
+      if( count > mostProducts ){
+        mostProducts = count;
+        savedProductKeys = [key]; //re-initialize saved key array
+      } else if( count === mostProducts ){
+        savedProductKeys.push(key); //add key to array
+      }
+    }
 
   return(
     <div className="User">
-    <h2>There are {mostUsers} users in the following state(s):</h2>
-    <p>{savedKeys.join(' - ')}</p>
+      <h2>Data Statistics:</h2>
+      <h3>There are {mostUsers} users in the following state(s):</h3>
+      <p>{savedKeys.join(' - ')}</p>
+
+      <h3>There are {mostCompanies} companies in the following state(s):</h3>
+      <p>{savedCompanyKeys.join(' - ')}</p>
+
+      <h3>The most common material(s) used:</h3>
+      <p>{savedProductKeys.join(' - ')}</p>
     </div>
   )
 }//end of RenderStats
